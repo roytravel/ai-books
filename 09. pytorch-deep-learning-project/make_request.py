5e22c12e18fb5d736492944a99c0f4d51c1f846d
DESKTOP-K6DD1JI\roytravel <roytravel97@gmail.com>
2022-11-11 01:39:47 +0900

    Add example of model serving and model standardization

--- 09. pytorch-deep-learning-project/make_request.py
+++ 09. pytorch-deep-learning-project/make_request.py
@@ -2,11 +2,7 @@ import io
 import json
 import requests
 from PIL import Image
-
 from torchvision import transforms
-
-
-image = Image.open("./digit_image.jpg")
 
 def image_to_tensor(image):
     gray_image = transforms.functional.to_grayscale(image)
@@ -15,14 +11,12 @@ def image_to_tensor(image):
     input_image_tensor_norm = transforms.functional.normalize(input_image_tensor, (0.1302,), (0.3069,))
     return input_image_tensor_norm
 
+image = Image.open("./digit_image.jpg")
 image_tensor = image_to_tensor(image)
-
 dimensions = io.StringIO(json.dumps({'dims': list(image_tensor.shape)}))
 data = io.BytesIO(bytearray(image_tensor.numpy()))
 
-r = requests.post('http://localhost:8890/test',
-                  files={'metadata': dimensions, 'data' : data})
-
-response = json.loads(r.content)
+res = requests.post('http://localhost:8890/test', files={'metadata': dimensions, 'data' : data})
+response = json.loads(res.content)
 
 print("Predicted digit :", response)
